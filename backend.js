9358c7a6ec7de36e7f2c2a7005617c8463764e24
barearojo <barearojojuan@gmail.com>
2024-03-22 11:27:44 +0100

    document loaded

--- backend.js
+++ backend.js
@@ -1,5 +1,21 @@
 console.log("Welcome to notes app. This is app.js");
 
+document.addEventListener("DOMContentLoaded",function(){
+  let addBtn = document.getElementById("addBtn"); //let en JavaScript se utiliza para declarar una variable local en el ámbito de bloque. 
+  //búsqueda en el documento HTML actual en busca de un elemento con el ID "addBtn". Cuando se encuentra un elemento con ese ID,
+  // este código asigna una referencia a ese elemento al variable addBtn.
+
+  addBtn.addEventListener("click" , addText);   // Aquí pasamos una referencia a la función addText sin invocarla.
+});
+
+
+// Aquí definimos la función addText correctamente.
+function addText() {
+  // Código para manejar el evento aquí
+  let addTxt = document.getElementById("addTxt");
+  let notes = document.getElementById("notes");
+   
+};
 
 
 
