24906b33257dae876f486219bc75640a1ae7a0a1
barearojo <barearojojuan@gmail.com>
2024-03-25 20:22:26 +0100

    show notes funcionando

--- backend.js
+++ backend.js
@@ -43,13 +43,13 @@ function showNotes(){
   let html = "";
   notesObj.forEach(function (element, index){
     html += `
-            <div class = "noteCard my-2 mx-2 card" style ="width : 18rem>
-              <div class = "cardBody">
-                <h5 class ="cardTitle">Note ${index + 1}</h5>
-                <p class = "cardtext> ${element}</p>
-                <button class="btn btn-primary">Delete Note</button>
-              </div>
-            </div>`;
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text"> ${element}</p>
+                        <button class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
   });
   let notesElm = document.getElementById("notes");
   if (notesObj.length != 0) {
